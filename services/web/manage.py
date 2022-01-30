db9e96bc8796533678a56fe13042fcbf1ed0ac77
Sasha Kogan <postmoderndeath@gmail.com>
2022-01-30 13:32:03 +0300

    prettify db init

--- services/web/manage.py
+++ services/web/manage.py
@@ -2,23 +2,26 @@ from flask.cli import FlaskGroup
 from werkzeug.security import generate_password_hash
 from project import app, db, User, City, Region
 import pandas as pd
-import inspect
-from dataclasses import dataclass
 
-#data loading
-df = pd.read_csv('db.csv')
-idx_region = [{'id': k, 'name': v} for k, v in enumerate(list(df['region'].unique()))]
+class LoadFromCSV:
+    def __init__(self, df):
+        self.df = df
+        self.generate_city = []
+        self.idx_region = [{'id': k, 'name': v} for k, v in enumerate(list(df['region'].unique()))]
 
-generate_cities = []
-for i, j in df.iterrows():
-    region_id = [item['id'] for item in idx_region if item["name"] == df.at[i, 'region']][0]
-    it = {'id': i, 'name': df.at[i, 'city'], 'region_id': region_id}
-    generate_cities.append(it)
+    def generate_cities(self):
+        for i, j in self.df.iterrows():
+            region_id = [item['id'] for item in self.idx_region if item["name"] == self.df.at[i, 'region']][0]
+            self.generate_city.append({'id': i, 'name': self.df.at[i, 'city'], 'region_id': region_id})
 
-form_regions_cities = []
-for i in idx_region:
-    city_list = [j for j in generate_cities if j['region_id'] == i['id']]
-    form_regions_cities.append({'id': i['id'],'name': i['name']})
+        return self.generate_city
+
+    def generate_regions(self):
+        return [{'id': i['id'],'name': i['name']} for i in self.idx_region]
+
+
+DBFiller = LoadFromCSV(pd.read_csv('db.csv'))
+generate_cities, form_regions_cities = DBFiller.generate_cities(), DBFiller.generate_regions()
 
 users = [
     {'id': 1, 'name': 'usr1', 'password': generate_password_hash('123')},
